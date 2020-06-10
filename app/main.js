a5e0201af998c9cb6e501a9c6518a4f249694fcc
Brandon Pittman <github@brandonpittman.net>
2020-06-10 22:20:40 +0900

    wip

--- app/main.js
+++ app/main.js
@@ -1,39 +1,41 @@
-const fs = require('fs');
-const { app, BrowserWindow, dialog } = require('electron');
+const fs = require("fs");
+const { app, BrowserWindow, dialog, Menu, MenuItem } = require("electron");
 
 let mainWindow = null;
 
-app.on('ready', () => {
-  console.log('App ready...');
+app.on("ready", () => {
+  console.log("App ready...");
 
   mainWindow = new BrowserWindow({ show: false, width: 1024, height: 800 });
 
+  Menu.setApplicationMenu(applicationMenu);
+
   mainWindow.loadFile(`${__dirname}/index.html`);
 
-  mainWindow.once('ready-to-show', () => {
+  mainWindow.once("ready-to-show", () => {
     mainWindow.show();
   });
 });
 
-console.log('Starting up...');
+console.log("Starting up...");
 
 exports.getFileFromUser = () => {
   const files = dialog.showOpenDialog(mainWindow, {
-    properties: ['openFile'],
+    properties: ["openFile"],
     filters: [
       {
-        name: 'Markdown Files',
-        extensions: ['md', 'markdown', 'mdown']
+        name: "Markdown Files",
+        extensions: ["md", "markdown", "mdown"],
       },
       {
-        name: 'Text Files',
-        extensions: ['txt', 'text']
+        name: "Text Files",
+        extensions: ["txt", "text"],
       },
       {
-        name: 'All Files',
-        extensions: ['*']
-      }
-    ]
+        name: "All Files",
+        extensions: ["*"],
+      },
+    ],
   });
 
   if (!files) return;
@@ -43,23 +45,23 @@ exports.getFileFromUser = () => {
   openFile(file);
 };
 
-const openFile = (exports.openFile = file => {
+const openFile = (exports.openFile = (file) => {
   const content = fs.readFileSync(file).toString();
   app.addRecentDocument(file);
-  mainWindow.webContents.send('file-opened', file, content);
+  mainWindow.webContents.send("file-opened", file, content);
 });
 
 exports.saveMarkdown = (file, content) => {
   if (!file) {
     file = dialog.showSaveDialog(mainWindow, {
-      title: 'Save Markdown',
-      defaultPath: app.getPath('desktop'),
+      title: "Save Markdown",
+      defaultPath: app.getPath("desktop"),
       filters: [
         {
-          name: 'Markdown Files',
-          extensions: ['md']
-        }
-      ]
+          name: "Markdown Files",
+          extensions: ["md"],
+        },
+      ],
     });
   }
 
@@ -69,12 +71,85 @@ exports.saveMarkdown = (file, content) => {
   openFile(file);
 };
 
-exports.saveHtml = html => {
+exports.saveHtml = (html) => {
   file = dialog.showSaveDialog(mainWindow, {
-    title: 'Save as HTML',
-    defaultPath: app.getPath('desktop'),
-    filters: [{ name: 'HTML Files', extensions: ['html'] }]
+    title: "Save as HTML",
+    defaultPath: app.getPath("desktop"),
+    filters: [{ name: "HTML Files", extensions: ["html"] }],
   });
 
   fs.writeFileSync(file, html);
 };
+
+const applicationName = "Fire Sale";
+const isMac = process.platform === "darwin";
+
+const template = [
+  { role: "appMenu" },
+  { role: "fileMenu" },
+  {
+    label: "Actions",
+    submenu: [
+      {
+        label: "New File",
+        accelerator: "CommandOrControl+N",
+        click() {
+          mainWindow.webContents.send("new-file");
+        },
+      },
+      {
+        label: "Open File",
+        accelerator: "CommandOrControl+O",
+        click() {
+          exports.getFileFromUser();
+        },
+      },
+      {
+        label: "Save File",
+        accelerator: "CommandOrControl+S",
+        click() {
+          mainWindow.webContents.send("save-markdown");
+        },
+      },
+      {
+        label: "Export HTML",
+        accelerator: "CommandOrControl+E",
+        click() {
+          mainWindow.webContents.send("export-html");
+        },
+      },
+      {
+        label: "Revert",
+        accelerator: "CommandOrControl+R",
+        click() {
+          mainWindow.webContents.send("revert");
+        },
+      },
+      {
+        label: "Reveal in Finder",
+        accelerator: "CommandOrControl+Shift+R",
+        click() {
+          mainWindow.webContents.send("reveal-in-finder");
+        },
+      },
+      {
+        label: "Open in Default Application",
+        accelerator: "CommandOrControl+Shift+O",
+        click() {
+          mainWindow.webContents.send("open-in-default-app");
+        },
+      },
+    ],
+  },
+  {
+    role: "editMenu",
+  },
+  {
+    role: "viewMenu",
+  },
+  {
+    role: "windowMenu",
+  },
+];
+
+const applicationMenu = Menu.buildFromTemplate(template);
--- app/renderer.js
+++ app/renderer.js
@@ -1,109 +1,146 @@
-const path = require('path');
-const { remote, ipcRenderer, shell } = require('electron');
-const main = remote.require('./main');
+const path = require("path");
+const { remote, ipcRenderer, shell } = require("electron");
+const main = remote.require("./main");
 const { openFile, getFileFromUser, saveMarkdown, saveHtml } = main;
-const marked = require('marked');
+const marked = require("marked");
 const currentWindow = remote.getCurrentWindow();
 
-let appTitle = 'Fire Sale';
+let appTitle = "Fire Sale";
 let filePath = null;
-let originalContent = '';
-let defaultExtension = 'md';
+let originalContent = "";
+let defaultExtension = "md";
 
-const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
-const newFileButton = document.querySelector('#new-file');
-const openFileButton = document.querySelector('#open-file');
-const saveMarkdownButton = document.querySelector('#save-markdown');
-const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('#save-html');
-const showFileButton = document.querySelector('#show-file');
-const openInDefaultButton = document.querySelector('#open-in-default');
+const markdownView = document.querySelector("#markdown");
+const htmlView = document.querySelector("#html");
+// const newFileButton = document.querySelector("#new-file");
+// const openFileButton = document.querySelector("#open-file");
+// const saveMarkdownButton = document.querySelector("#save-markdown");
+// const revertButton = document.querySelector("#revert");
+// const saveHtmlButton = document.querySelector("#save-html");
+// const showFileButton = document.querySelector("#show-file");
+// const openInDefaultButton = document.querySelector("#open-in-default");
 
-document.addEventListener('dragstart', e => e.preventDefault());
-document.addEventListener('dragleave', e => e.preventDefault());
-document.addEventListener('dragover', e => e.preventDefault());
-document.addEventListener('drop', e => e.preventDefault());
+document.addEventListener("dragstart", (e) => e.preventDefault());
+document.addEventListener("dragleave", (e) => e.preventDefault());
+document.addEventListener("dragover", (e) => e.preventDefault());
+document.addEventListener("drop", (e) => e.preventDefault());
 
 const cleanUpClasses = () => {
-  markdownView.classList.add('bg-purple-100');
-  markdownView.classList.remove('bg-green-200');
-  markdownView.classList.remove('bg-red-200');
+  markdownView.classList.add("bg-purple-100");
+  markdownView.classList.remove("bg-green-200");
+  markdownView.classList.remove("bg-red-200");
 };
-const getDraggedFile = event => event.dataTransfer.items[0];
-const getDroppedFile = event => event.dataTransfer.files[0];
-const fileTypeIsSupported = file =>
-  ['text/plain', 'text/markdown'].includes(file.type);
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+const fileTypeIsSupported = (file) =>
+  ["text/plain", "text/markdown"].includes(file.type);
 
-markdownView.addEventListener('dragover', event => {
+markdownView.addEventListener("dragover", (event) => {
   const file = getDraggedFile(event);
 
   if (fileTypeIsSupported(file)) {
-    markdownView.classList.remove('bg-purple-100');
-    markdownView.classList.add('bg-green-200');
+    markdownView.classList.remove("bg-purple-100");
+    markdownView.classList.add("bg-green-200");
   } else {
-    markdownView.classList.remove('bg-purple-100');
-    markdownView.classList.add('bg-red-200');
+    markdownView.classList.remove("bg-purple-100");
+    markdownView.classList.add("bg-red-200");
   }
 });
 
-markdownView.addEventListener('dragleave', () => cleanUpClasses());
+markdownView.addEventListener("dragleave", () => cleanUpClasses());
 
-markdownView.addEventListener('drop', event => {
+markdownView.addEventListener("drop", (event) => {
   const file = getDroppedFile(event);
 
   if (fileTypeIsSupported(file)) {
     openFile(file.path);
   } else {
-    alert('File type not supported.');
+    alert("File type not supported.");
   }
   cleanUpClasses();
 });
 
-const renderMarkdownToHtml = markdown => {
+const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
-revertButton.addEventListener('click', () => {
-  const result = confirm('Are you sure you want to revert?');
+const revertDocument = () => {
+  const result = confirm("Are you sure you want to revert?");
   if (result) {
     markdownView.value = originalContent;
     renderMarkdownToHtml(originalContent);
     updateUserInterface(false);
   }
-});
+};
 
-markdownView.addEventListener('keyup', event => {
+const newFile = () => {
+  let result = true;
+
+  if (markdownView.value !== originalContent || filePath) {
+    result = confirm("Are you sure you want to create a new file?");
+  }
+
+  if (result) {
+    filePath = null;
+    markdownView.value = "";
+    renderMarkdownToHtml(markdownView.value);
+    updateUserInterface(false);
+  }
+};
+
+ipcRenderer.on("new-file", newFile);
+
+// revertButton.addEventListener("click", revertDocument);
+
+ipcRenderer.on("revert", revertDocument);
+
+markdownView.addEventListener("keyup", (event) => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
   updateUserInterface(currentContent !== originalContent);
 });
 
-openFileButton.addEventListener('click', () => {
-  getFileFromUser();
-});
+// openFileButton.addEventListener("click", () => {
+//   getFileFromUser();
+// });
 
-saveHtmlButton.addEventListener('click', () => {
+const exportHtml = () => {
   let html = htmlView.innerHTML;
   saveHtml(html);
-});
+};
 
-saveMarkdownButton.addEventListener('click', () => {
+// saveHtmlButton.addEventListener("click", exportHtml);
+
+ipcRenderer.on("export-html", exportHtml);
+
+const saveAsMarkdown = () => {
   let currentContent = markdownView.value;
   saveMarkdown(filePath, currentContent);
   originalContent = currentContent;
   updateUserInterface();
-});
+};
 
-showFileButton.addEventListener('click', event => {
-  if (!filePath) return alert('Nope!');
+ipcRenderer.on("save-markdown", saveAsMarkdown);
+
+// saveMarkdownButton.addEventListener("click", saveAsMarkdown);
+
+const revealInFinder = () => {
+  if (!filePath) return alert("Nope!");
   shell.showItemInFolder(filePath);
-});
+};
 
-openInDefaultButton.addEventListener('click', event => {
-  if (!filePath) return alert('Nope!');
+// showFileButton.addEventListener("click", revealInFinder);
+
+ipcRenderer.on("reveal-in-finder", revealInFinder);
+
+const openInDefaultApp = () => {
+  if (!filePath) return alert("Nope!");
   shell.openItem(filePath);
-});
+};
+
+// openInDefaultButton.addEventListener("click", openInDefaultApp);
+
+ipcRenderer.on("open-in-default-app", openInDefaultApp);
 
 const updateUserInterface = (isDirty = false) => {
   let title = appTitle;
@@ -120,9 +157,10 @@ const updateUserInterface = (isDirty = false) => {
   revertButton.disabled = !isDirty;
   showFileButton.disabled = !filePath;
   openInDefaultButton.disabled = !filePath;
+  markdownView.autofocus();
 };
 
-ipcRenderer.on('file-opened', (_event, file, content) => {
+ipcRenderer.on("file-opened", (_event, file, content) => {
   filePath = file;
   originalContent = content;
   markdownView.value = content;
